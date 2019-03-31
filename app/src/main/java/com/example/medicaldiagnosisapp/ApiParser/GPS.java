3db370ea62f248f35f398552adc2d77940b9c57b
tansr7 <tansr2010@gmail.com>
2019-04-01 05:43:33 +0800

    add poly,chas,aed map fragments. crash when exiting gmap frag

--- app/src/main/java/com/example/medicaldiagnosisapp/ApiParser/GPS.java
+++ app/src/main/java/com/example/medicaldiagnosisapp/ApiParser/GPS.java
@@ -1,8 +1,11 @@
-/*
-
 package com.example.medicaldiagnosisapp.ApiParser;
 
+
+
+/*
+import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -14,14 +17,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class getCurrentLocation extends FragmentActivity
-        implements LocationListener
-{
-    {
-    LocationManager locationManager;
-    MapView mMap;
+public class getCurrentLocation implements LocationListener {
 
-    @Override
+*/
+/*    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locate_aed);
@@ -42,27 +41,49 @@ public class getCurrentLocation extends FragmentActivity
 
         mMap = (MapView) findViewById(R.id.mapDisplay);
 
+    }*//*
+
+
+    public static void initialise () {
+        LocationManager locationManager;
+        locationManager = (LocationManager) getSystemService(getApplicationContext().LOCATION_SERVICE);
+*/
+/*        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }*//*
+
+        Location location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
+        onLocationChanged(location);
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        double currLong = location.getLongitude();
-        double currLat = location.getLatitude();
+        LatLng currLocat = new LatLng(location.getLatitude(), location.getLongitude());
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-
+        //This method was deprecated in API level Q.
+        //This callback will never be invoked.
     }
 
     @Override
     public void onProviderEnabled(String provider) {
-
+        //not needed
     }
 
     @Override
     public void onProviderDisabled(String provider) {
-
+        //not needed
     }
 }
+
+
 */
