8e29415380970e948465b5bed595803735640863
kendrachan <kendrachanst@gmail.com>
2019-04-03 18:39:43 +0800

    NEWEDITPLSWORK

--- app/src/main/java/com/example/medicaldiagnosisapp/FinalDiagnosisActivity.java
+++ app/src/main/java/com/example/medicaldiagnosisapp/FinalDiagnosisActivity.java
@@ -1,7 +1,11 @@
 package com.example.medicaldiagnosisapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class FinalDiagnosisActivity extends AppCompatActivity {
 
@@ -9,5 +13,43 @@ public class FinalDiagnosisActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_diagnosis);
+
+        //Bottom Navigation Start//
+
+        BottomNavigationView btmNavMenu = (BottomNavigationView) findViewById(R.id.btm_navigation_menu);
+
+        btmNavMenu.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.nav_action_info:
+                                Intent moveToInfo = new Intent(getApplicationContext(), InformationActivity.class);
+                                startActivity(moveToInfo);
+                                break;
+                            case R.id.nav_action_call:
+                                Intent moveToContact = new Intent(getApplicationContext(), ContactActivity.class);
+                                startActivity(moveToContact);
+                                break;
+                            case R.id.nav_action_diagnose:
+                                Intent moveToDiagnose1 = new Intent(getApplicationContext(), DiagnoseActivityPage1.class);
+                                startActivity(moveToDiagnose1);
+                                break;
+                            case R.id.nav_action_locate:
+                                Intent moveToLocate = new Intent(getApplicationContext(), LocateActivity.class);
+                                startActivity(moveToLocate);
+                                break;
+                            case R.id.nav_action_help:
+                                Intent moveToHelp = new Intent(getApplicationContext(), HelpActivity.class);
+                                startActivity(moveToHelp);
+                                break;
+                        }
+                        return true;
+                    }
+
+                });
+
+        //Bottom navigation end//
+
     }
 }
