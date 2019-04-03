23e7b7729cf2a17a1973161fc683c882e5018361
Tan Sheng Rong <31932046+tansr7@users.noreply.github.com>
2019-04-04 01:35:11 +0800

    remove current location usage, adjusted zoom.

--- app/src/main/java/com/example/medicaldiagnosisapp/AdminActivityFinal.java
+++ app/src/main/java/com/example/medicaldiagnosisapp/AdminActivityFinal.java
@@ -4,20 +4,16 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -29,13 +25,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class AdminActivityFinal extends AppCompatActivity implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
 
@@ -45,7 +43,6 @@ public class AdminActivityFinal extends AppCompatActivity implements OnMapReadyC
 
     private static final int LOCATION_REQUEST_CODE = 991;
     private GoogleMap mMap;
-    private FusedLocationProviderClient mFusedLocationClient;
     private ArrayList<Marker> mMarkerArray = new ArrayList<Marker>();
 
     @Override
@@ -62,8 +59,6 @@ public class AdminActivityFinal extends AppCompatActivity implements OnMapReadyC
         String[] startEndDate = (String[]) adminView.get("Start/End Date");
         final ArrayList<String> condition = (ArrayList<String>) adminView.get("Conditions");
         final ArrayList<String> regions = (ArrayList<String>) adminView.get("Regions");
-        final String startDate = startEndDate[0];
-        final String endDate = startEndDate[1];
 
         Query query = logRef.orderByChild("date").startAt(startEndDate[0]).endAt(startEndDate[1]);
 
@@ -96,7 +91,6 @@ public class AdminActivityFinal extends AppCompatActivity implements OnMapReadyC
                 }
                 //Dynamic text view
                 TextView totalCase = (TextView) findViewById(R.id.textViewTotalCase);
-                Log.d("size", "Confound" + String.valueOf(mMarkerArray.size()));
                 totalCase.setText("Total Number of Diagnosis: " + String.valueOf(mMarkerArray.size()));
             }
 
@@ -140,8 +134,6 @@ public class AdminActivityFinal extends AppCompatActivity implements OnMapReadyC
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
     }
 
     //Gmap View
@@ -151,15 +143,6 @@ public class AdminActivityFinal extends AppCompatActivity implements OnMapReadyC
         if (requestCode == LOCATION_REQUEST_CODE) {
             if (permissions.length > 0 &&
                     grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                mMap.setMyLocationEnabled(true);
-                mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        if (location != null) {
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 15));
-                        }
-                    }
-                });
                 Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
@@ -170,8 +153,7 @@ public class AdminActivityFinal extends AppCompatActivity implements OnMapReadyC
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -188,7 +170,7 @@ public class AdminActivityFinal extends AppCompatActivity implements OnMapReadyC
         mMap.setMinZoomPreference(10.0f);
         mMap.setMaxZoomPreference(16.0f);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(1.35, 103.8), 12));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(1.35, 103.8), 11));
 
         LatLngBounds SINGAPORE = new LatLngBounds(
                 new LatLng(1.152761, 103.559083), new LatLng(1.487512, 104.113698));
@@ -197,74 +179,7 @@ public class AdminActivityFinal extends AppCompatActivity implements OnMapReadyC
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_REQUEST_CODE);
-        } else {
-            mMap.setMyLocationEnabled(true);
-            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if (location != null) {
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 10));
-                    }
-                }
-            });
         }
-
-    }
-
-    public void buttonClick(View v) {
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_REQUEST_CODE);
-        } else {
-            mMap.setMyLocationEnabled(true);
-            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if (location != null && !mMarkerArray.isEmpty()) {
-                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(findNearest(location), 15.0f));
-                    }
-                }
-            });
-        }
-
-    }
-
-    private LatLng findNearest(Location myLoc) {
-        LatLng nearestLoc = new LatLng(0, 0);
-        LatLng myLatLng = new LatLng(myLoc.getLatitude(), myLoc.getLongitude());
-//        Log.d(TAG, "my latlng = " + myLatLng.latitude + ", " + myLatLng.longitude);
-        Marker nearestMark = null;
-
-        for (Marker marker : mMarkerArray) {
-            LatLng temp = new LatLng(marker.getPosition().latitude, marker.getPosition().longitude);
-//            Log.d(TAG, "latlng = " + temp.latitude + ", " + temp.longitude);
-            if (getDistance(temp, myLatLng) <= getDistance(nearestLoc, myLatLng)) {
-                nearestLoc = temp;
-//                Log.d(TAG, "current nearest latlng = " + nearestLoc.latitude + ", " + nearestLoc.longitude);
-                nearestMark = marker;
-            }
-        }
-//        Log.d(TAG, "my latlng = " + myLatLng.latitude + ", " + myLatLng.longitude);
-
-//        Log.d(TAG, "actual nearest latlng = " + nearestLoc.latitude + ", " + nearestLoc.longitude);
-        if (nearestMark != null) {
-            nearestMark.showInfoWindow();
-        }
-        return nearestLoc;
-    }
-
-    public double getDistance(LatLng LatLng1, LatLng LatLng2) {
-        double distance = 0;
-        Location locationA = new Location("A");
-        locationA.setLatitude(LatLng1.latitude);
-        locationA.setLongitude(LatLng1.longitude);
-        Location locationB = new Location("B");
-        locationB.setLatitude(LatLng2.latitude);
-        locationB.setLongitude(LatLng2.longitude);
-        distance = locationA.distanceTo(locationB);
-//        Log.d(TAG, "distance = " + distance);
-        return distance;
     }
 
     public class MyInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
@@ -278,7 +193,7 @@ public class AdminActivityFinal extends AppCompatActivity implements OnMapReadyC
         @Override
         public View getInfoContents(Marker marker) {
 
-            TextView tvTitle = ((TextView) myContentsView.findViewById(R.id.emergency_service));
+            TextView tvTitle = ((TextView) myContentsView.findViewById(R.id.emergency_service)); //just reuse existing
             tvTitle.setText(marker.getTitle());
             TextView tvSnippet = ((TextView) myContentsView.findViewById(R.id.snippet));
             tvSnippet.setText(marker.getSnippet());
