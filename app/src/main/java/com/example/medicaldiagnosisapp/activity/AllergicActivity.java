2dced15bf21ba6e9317e2c8ea1753537afdc9f30
tansr7 <tansr2010@gmail.com>
2019-04-08 03:11:51 +0800

    remove commented old taskbar

--- app/src/main/java/com/example/medicaldiagnosisapp/activity/AllergicActivity.java
+++ app/src/main/java/com/example/medicaldiagnosisapp/activity/AllergicActivity.java
@@ -18,51 +18,6 @@ public class AllergicActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_allergic);
 
-        /*Toolbar Buttons Start Here
-
-        Button diagnoseButton = (Button) findViewById(R.id.diagnoseButton);
-        diagnoseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent moveToDiagnose1 = new Intent(getApplicationContext(), DiagnoseActivityPage1.class);
-                startActivity(moveToDiagnose1);
-            }
-        });
-
-        ImageButton helpButton =  findViewById(R.id.helpButton);
-        helpButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent moveToHelp = new Intent(getApplicationContext(),HelpActivity.class);
-                startActivity(moveToHelp);
-            }
-        });
-        ImageButton locateButton =  findViewById(R.id.locateButton);
-        locateButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent moveToLocate = new Intent(getApplicationContext(),LocateActivity.class);
-                startActivity(moveToLocate);
-            }
-        });
-        ImageButton contactPageButton = findViewById(R.id.contactButton);
-        contactPageButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent moveToContact = new Intent(getApplicationContext(),ContactActivity.class);
-                startActivity(moveToContact);
-            }
-        });
-        ImageButton infoButton =  findViewById(R.id.infoButton);
-        infoButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent moveToInfo = new Intent(getApplicationContext(),InformationActivity.class);
-                startActivity(moveToInfo);
-            }
-        });
-        //Toolbar Buttons End Here*/
-
         //Bottom Navigation Start//
 
         BottomNavigationView btmNavMenu = (BottomNavigationView) findViewById(R.id.btm_navigation_menu);
