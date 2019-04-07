0c625ef674b35f43c96625caf25faf54359af044
tansr7 <tansr2010@gmail.com>
2019-04-08 03:07:52 +0800

    first try at javadocs

--- app/src/main/java/com/example/medicaldiagnosisapp/activity/BoneActivity.java
+++ app/src/main/java/com/example/medicaldiagnosisapp/activity/BoneActivity.java
@@ -1,15 +1,16 @@
 package com.example.medicaldiagnosisapp.activity;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.medicaldiagnosisapp.R;
+
 
 public class BoneActivity extends AppCompatActivity {
 
